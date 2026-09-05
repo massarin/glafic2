73f6fa169323a584e02f5cb398708199f37778c4
Nicolo Massari <nicolo.massari@protonmail.com>
2026-09-05 22:30:56 +0200

    chore: make glafic2 a python project

--- setup.py
+++ setup.py
@@ -1,39 +1,14 @@
-import os
-import sys
 import subprocess
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.build_py import build_py
 
-# here = os.path.abspath(os.path.dirname(__file__))
 
-def build_extensions():
-    subprocess.run(
-        "make clean; make python",
-        shell = True,
-        check = True,
-    )
+# metadata lives in pyproject.toml; this shim only drives the Makefile build of glafic.so
+class BuildPy(build_py):
+    def run(self):
+        subprocess.run("make clean; make python", shell = True, check = True)
+        super().run()
 
 
-def make_cmd_class(base):
-    class CmdClass(base):
-        def run(self):
-            build_extensions()
-            super().run()
-
-    return CmdClass
-
-setup(
-    name = 'glafic',
-    version = '2.1.15',
-    author = 'Masamune Oguri',
-    description = 'Library for gravitational lensing analyses',
-    url = 'https://github.com/oguri/glafic2',
-    packages = find_packages(where = 'python'),
-    package_dir = {'': 'python'},
-    python_requires = '>=3',
-    package_data = {'glafic': ['*.so']},
-    cmdclass = {
-       'build_py': make_cmd_class(build_py)
-    },
-)
+setup(cmdclass = {'build_py': BuildPy})
